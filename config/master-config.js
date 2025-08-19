4dbd6760748f8436cb4279fa1eb57df8959c445d
anglo911 <zxxx1227@gmail.com>
2025-08-19 19:36:56 +0800

    Update master-config.js

--- config/master-config.js
+++ config/master-config.js
@@ -6,7 +6,7 @@ const MASTER_CONFIG = {
     auth: {
         username: 'admin',                    // 用户名
         password: '858929',            // 🔥 修改这里设置自定义密码
-        enabled: true,                        // 是否启用密码保护
+        enabled: false,                        // 是否启用密码保护
         sessionDuration: 90 * 24 * 60 * 60 * 1000,  // 90天
         maxLoginAttempts: 5,                  // 最大尝试次数
         lockoutDuration: 30 * 60 * 1000       // 锁定时间30分钟
@@ -260,3 +260,4 @@ if (typeof window !== 'undefined' && (window.location.hostname === 'localhost' |
     console.log('🌐 代理调试:', MASTER_CONFIG.proxy.debug ? '已启用' : '已禁用');
 
 }
+
